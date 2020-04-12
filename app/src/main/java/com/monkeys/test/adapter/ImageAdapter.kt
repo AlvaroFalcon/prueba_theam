203629e7d20cc1c4fea204f1253b3866fe003a00
Alvaro Falcon Morales <alvaro@2coders.com>
2020-04-13 00:40:01 +0100

    Set size of images in product carrousel

--- app/src/main/java/com/monkeys/test/adapter/ImageAdapter.kt
+++ app/src/main/java/com/monkeys/test/adapter/ImageAdapter.kt
@@ -1,8 +1,10 @@
 package com.monkeys.test.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.monkeys.test.R
 import kotlinx.android.synthetic.main.product_image_item.view.*
@@ -11,6 +13,10 @@ class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
     private var itemList: Array<String> = arrayOf()
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    companion object{
+        const val IMAGES_IN_SCREEN = 2.5f
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(getView(),parent,false)
@@ -26,7 +32,22 @@ class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
+        setImageSize(context, holder)
         holder.itemView.image.setImageResource(R.drawable.placeholder)
+    }
+
+    private fun setImageSize(
+        context: Context,
+        holder: ViewHolder
+    ) {
+        val screenWidth = context.resources.displayMetrics.widthPixels
+        val margin = context.resources.getDimension(R.dimen.material_horizontal_margin)
+        val imgWidth = (screenWidth / IMAGES_IN_SCREEN) - (margin * 2)
+        val params =
+            LinearLayout.LayoutParams(imgWidth.toInt(), LinearLayout.LayoutParams.WRAP_CONTENT)
+        params.marginEnd = margin.toInt()
+        holder.itemView.image.layoutParams = params
     }
 
     fun refreshData(images: Array<String>) {

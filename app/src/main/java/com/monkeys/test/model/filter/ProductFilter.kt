3021fb2c0cd239d49fb90a9e62a418ea48c7c56d
Alvaro Falcon Morales <alvaro@2coders.com>
2020-04-19 18:25:00 +0100

    Added missing filters, priceMin and priceMax

--- app/src/main/java/com/monkeys/test/model/filter/ProductFilter.kt
+++ app/src/main/java/com/monkeys/test/model/filter/ProductFilter.kt
@@ -2,4 +2,13 @@ package com.monkeys.test.model.filter
 
 import java.io.Serializable
 
-class ProductFilter(val storeId: Int, val categoryId: Int, val order: FilterOrder? = null, val page: Int = 1, val limit: Int = 10, val text: String? = null): Serializable
+class ProductFilter(
+    val storeId: Int,
+    val categoryId: Int,
+    val order: FilterOrder? = null,
+    val page: Int = 1,
+    val limit: Int = 10,
+    val text: String? = null,
+    val priceMin: Int? = null,
+    val priceMax: Int? = null
+) : Serializable

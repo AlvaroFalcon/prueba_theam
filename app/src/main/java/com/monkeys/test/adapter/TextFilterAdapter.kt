f70bcd42d1aa7b7f78ce8287a74ee6101fd751bd
Alvaro Falcon Morales <alvaro@2coders.com>
2020-04-24 00:47:09 +0100

    Making textFilters selectable

--- app/src/main/java/com/monkeys/test/adapter/TextFilterAdapter.kt
+++ app/src/main/java/com/monkeys/test/adapter/TextFilterAdapter.kt
@@ -3,6 +3,7 @@ package com.monkeys.test.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.monkeys.test.R
 import com.monkeys.test.model.filter.text_filter.TextFilterOption
@@ -27,7 +28,16 @@ class TextFilterAdapter : RecyclerView.Adapter<TextFilterAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.text.text  = itemList[position].label
+        val item = itemList[position]
+        val context = holder.itemView.context
+        val drawable = ContextCompat.getDrawable(context, if(item.selected) R.drawable.rounded_background_outline_selected else R.drawable.rounded_background_outline)
+        holder.itemView.text.text  = item.label
+        holder.itemView.background = drawable
+        holder.itemView.setOnClickListener {
+            item.selected = !item.selected
+            notifyItemChanged(position)
+        }
+
     }
 
     fun refreshData(items: Array<TextFilterOption>) {
--- app/src/main/java/com/monkeys/test/model/filter/text_filter/TextFilterOption.kt
+++ app/src/main/java/com/monkeys/test/model/filter/text_filter/TextFilterOption.kt
@@ -2,4 +2,4 @@ package com.monkeys.test.model.filter.text_filter
 
 import java.io.Serializable
 
-class TextFilterOption (val id: String, val label: String, selected: Boolean? = false): Serializable
+class TextFilterOption (val id: String, val label: String, var selected: Boolean = false): Serializable
